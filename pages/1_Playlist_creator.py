12023b6a2a81ca2bbe1b003311f86f2d35de4da2
miquelLlorca <mlb51@gcloud.ua.es>
2025-02-21 15:15:36 +0100

    Added album extractor

--- pages/1_Playlist_creator.py
+++ pages/1_Playlist_creator.py
@@ -6,6 +6,10 @@ import pandas as pd
 import streamlit as st
 from spotipy.oauth2 import SpotifyOAuth
 
+PLAYLIST = 'Personal playlist'
+ALBUM = 'Album'
+
+
 def get_spotify_credentials():
     ''' Loads spotify credentials. Returns [id, secret]'''
     with open('credentials.json', 'r') as file:
@@ -16,17 +20,11 @@ def get_spotify_credentials():
 def get_playlist_data(sp, playlist_id, filename):
     ''' Uses the spotify API to get all data from a playlist.
         If the data is already available locally it skips the call.'''
-    
-    raw_data_path = f'data/spotify_raw/{filename}'
-    if(os.path.exists(raw_data_path) and not data.check_too_old(raw_data_path)):
-        st.text('Data already downloaded, skipping API call...')
-        return data.read_as_df(raw_data_path)
-    
-    # if the file does not exist or is older it gets the data again
+
     all_tracks = []
     offset = 0
     limit = 100
-
+    raw_data_path = f'data/spotify_raw/{filename}'
     while True:
         try:
             response = sp.playlist_items(playlist_id, offset=offset, limit=limit)
@@ -58,14 +56,67 @@ def get_playlist_data(sp, playlist_id, filename):
             
             offset += limit
         except Exception as e:
-            print(f"Error: {e}")
+            st.text(f"Error: {e}")
             break
 
     df = pd.DataFrame(all_tracks)
     data.save_df(df, raw_data_path)
     return df
 
+def get_album_data(sp, album_id, filename):
+    ''' Uses the Spotify API to get all data from an album.
+        If the data is already available locally, it skips the call.'''
+   
+    raw_data_path = f'data/spotify_raw/{filename}'
+    try:
+        album = sp.album(album_id)
+        tracks = album['tracks']['items']
+        all_tracks = []
 
+        for track in tracks:
+            artist_ids = [artist['id'] for artist in track['artists']]
+            genres = []
+            for artist_id in artist_ids:
+                artist_info = sp.artist(artist_id)
+                genres.extend(artist_info.get('genres', []))
+
+            track_info = {
+                'name': track['name'],
+                'artist': ', '.join(artist['name'] for artist in track['artists']),
+                'album': album['name'],
+                'release_date': album['release_date'],
+                'duration_ms': track['duration_ms'],
+                'popularity': track.get('popularity', None),
+                'genres': list(set(genres))  # Remove duplicates
+            }
+            all_tracks.append(track_info)
+
+        df = pd.DataFrame(all_tracks)
+        if(df.empty):
+            st.text('No data with this ID')
+        else:
+            data.save_df(df, raw_data_path)
+        return df
+
+    except Exception as e:
+        st.text(f"Error: {e}")
+        return None
+    
+def get_data(source, sp, playlist_id, filename):
+     
+    raw_data_path = f'data/spotify_raw/{filename}'
+    if os.path.exists(raw_data_path) and not data.check_too_old(raw_data_path):
+        st.text('Data already downloaded, skipping API call...')
+        return data.read_as_df(raw_data_path)
+
+    if(source==PLAYLIST):
+        return get_playlist_data(sp, playlist_id, filename)
+    elif(source==ALBUM):
+        return get_album_data(sp, playlist_id, filename)
+
+#####################################################################################################################################################
+#####################################################################################################################################################
+#####################################################################################################################################################
 
 if(__name__=="__main__"):
     CLIENT_ID, CLIENT_SECRET = get_spotify_credentials()
@@ -73,7 +124,8 @@ if(__name__=="__main__"):
     st.set_page_config(layout="wide")
     st.title("Playlist creator")
     st.text('Here you can get the data from any playlist on spotify, you can also update already downloaded playlists.')
-        
+    
+    source = st.selectbox(label='source', options=[PLAYLIST, ALBUM])
     playlist_id = st.text_input('playlist_id', value="6cR4y6y6ExPNk93BodOG56")
     playlist_name = st.text_input('playlist_name', value="techno_flow_state" )
     df = st.session_state.get('df')
@@ -93,7 +145,7 @@ if(__name__=="__main__"):
             st.text('Updating playlist data...')
             data.create_playlist_backup(path)
             df = data.read_as_df(path)
-            updated_df = get_playlist_data(sp, playlist_id, filename)
+            updated_df = get_data(source, sp, playlist_id, filename)
 
             # Prepares data for merge
             df.drop(columns=['genres', 'name', 'artist', 'album', 'popularity'], errors='ignore', inplace=True)
@@ -107,7 +159,7 @@ if(__name__=="__main__"):
             df = pd.merge(updated_df, df, on=['release_date', 'duration_ms'], how='left')
         else:
             st.text('Creating new playlist...')
-            df = get_playlist_data(sp, playlist_id, filename)
+            df = get_data(source, sp, playlist_id, filename)
             # Prepares data for possible future merge
             df['release_date'] = df['release_date'].apply(data.correct_date_format)
             df['release_date'] = pd.to_datetime(df['release_date'], errors='coerce')
