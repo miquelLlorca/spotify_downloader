7c1ba3bdeb70578b48b6493b5c9366254c1b6589
miquelLlorca <mlb51@gcloud.ua.es>
2025-03-31 19:41:11 +0200

    Added yt-dlp downloader

--- pages/3_Downloader.py
+++ pages/3_Downloader.py
@@ -4,7 +4,7 @@ import streamlit as st
 import plots
 
 Y2MATE = 'y2mate'
-NOTUBE = 'notube'
+YTDLP = 'yt-dlp'
 
 if(__name__=='__main__'):
     st.set_page_config(layout="wide")
@@ -13,17 +13,28 @@ if(__name__=='__main__'):
     st.text('There are 2 options, the recommended one is y2mate, notube is not working for now as it has captcha verifitcation.')
 
     playlist_name = st.selectbox('Select a playlist', data.get_all_playlist_paths())
-    webpage = st.selectbox('Choose an option:', [Y2MATE, NOTUBE], index=0)
+    webpage = st.selectbox('Choose an option:', [Y2MATE, YTDLP], index=0)
     path = f'data/{playlist_name}'
     st.plotly_chart(plots.plot_song_state_pie(data.read_as_df(path)))
     
     if(st.button('Start downloading')):
-        with st.spinner("Launching scraper..."):
-            subprocess.Popen(
-                ["python", "scripts/downloader.py", "--path", path, '--webpage', webpage],
-                    stdout=None, # Does not redirect outputs
-                    stderr=None,
-                    start_new_session=True  # Ensure it runs independently
-            )
-        st.success(f"Scraper finished for {playlist_name}!")
-        st.text('Check the results as errors are not caught here.')
+        if(webpage == Y2MATE):
+            with st.spinner("Launching scraper..."):
+                subprocess.Popen(
+                    ["python", "scripts/downloader.py", "--path", path, '--webpage', webpage],
+                        stdout=None, # Does not redirect outputs
+                        stderr=None,
+                        start_new_session=True  # Ensure it runs independently
+                )
+                st.success(f"Scraper finished for {playlist_name}!")
+            st.text('Check the results as errors are not caught here.')
+        elif(webpage==YTDLP):
+            with st.spinner("Launching scraper..."):
+                subprocess.Popen(
+                    ["python", "scripts/yt-dlp_downloader.py", "--path", path],
+                        stdout=None, # Does not redirect outputs
+                        stderr=None,
+                        start_new_session=True  # Ensure it runs independently
+                )
+                st.success(f"Scraper finished for {playlist_name}!")
+            st.text('Check the results as errors are not caught here.')
---
+++ scripts/yt-dlp_downloader.py
@@ -0,0 +1,56 @@
+
+
+import data
+from yt_dlp import YoutubeDL
+import pandas as pd
+import argparse
+import data
+
+
+def download_playlist(path):
+    downloaded_songs = []
+    def progress_hook(d):
+        if d["status"] == "finished":
+            url = d["info_dict"]["original_url"]
+            downloaded_songs.append(url)  # Store for later
+
+
+    options = {
+        "format": "bestaudio",
+        "outtmpl": "/home/miquel/Downloads/%(title)s.%(ext)s",
+        "progress_hooks": [progress_hook],
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "256",  # You can change the bitrate
+            }
+        ],
+    }
+
+
+
+    df = data.read_as_df(path)
+
+    if('downloaded' not in df.columns):
+        df['downloaded'] = [False for i in range(len(df))]
+        
+    try:
+        urls = df[~ df["downloaded"]]["YouTube_URL"].values
+        with YoutubeDL(options) as ydl:
+            ydl.download(urls)
+
+        df.loc[df["YouTube_URL"].isin(downloaded_songs), "downloaded"] = True
+        data.save_df(df, path)
+
+    except:
+        df.loc[df["YouTube_URL"].isin(downloaded_songs), "downloaded"] = True
+        data.save_df(df, path)
+
+
+
+if(__name__=='__main__'):
+    parser = argparse.ArgumentParser(description="Scraper script.")
+    parser.add_argument("--path", type=str, required=True, help="Path to playlist")
+    args = parser.parse_args()
+    download_playlist(args.path)
