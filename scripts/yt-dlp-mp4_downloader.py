05b8ef38b057fb5013bd4e5f9d81158a4f1abb49
miquelLlorca <mlb51@gcloud.ua.es>
2025-06-11 10:35:53 +0200

    Saved repo state to transfer to windows

--- scripts/yt-dlp-mp4_downloader.py
+++ scripts/yt-dlp-mp4_downloader.py
@@ -4,11 +4,11 @@
 from yt_dlp import YoutubeDL
 
 urls = [
-    'https://youtu.be/playlist?list=PLkPM73HZcYt03hiFlsLGZdIJdF9pYLFU3'
+    'https://www.youtube.com/watch?v=Q5TdYpS45eo&pp=ygUSZGVwYXJ0YW1lbnRvIGNvc2Fz'
 ]
 
+series_name = 'departamento_cosas'
 
-series_name = 'DHMIS'
 options = {
     "format": "bestvideo[height<=1080][ext=mp4]+bestaudio[ext=m4a]/best[height<=1080][ext=mp4]",
     "merge_output_format": "mp4",
